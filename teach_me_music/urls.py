6790e138de6bbdb36f516a5d31c7ef37699aacde
shmouel1824 <b747800613@gmail.com>
2026-05-03 10:00:05 +0300

    update

--- teach_me_music/urls.py
+++ teach_me_music/urls.py
@@ -3,11 +3,18 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
+from django.middleware.csrf import get_token
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def loading_view(request):
+    get_token(request)  # forces CSRF cookie to be set
+    return render(request, 'loading.html')
 
 urlpatterns = [
     path('admin/',      admin.site.urls),
     path('accounts/',   include('accounts.urls',   namespace='accounts')),
-    path('',            TemplateView.as_view(template_name='loading.html'), name='loading'),
+    path('',            loading_view, name='loading'),
     path('play/',       include('exercises.urls',   namespace='exercises')),
     path('api/',        include('recognition.urls', namespace='recognition')),
     path('dashboard/',  include('dashboard.urls',   namespace='dashboard')),
